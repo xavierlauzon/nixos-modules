3a8f09493da8a62f755f464c615dd9d5379ba9bd
Xavier Lauzon <xavier@lumae.net>
2024-11-27 18:15:07 -0500

    Use TCP BBR

--- modules/network/wired.nix
+++ modules/network/wired.nix
@@ -188,7 +188,13 @@ in {
           }
         ) unbridgedInterfaces);
 
-      in unbridgedNetworks // bridgedNetworks // bridgeNetwork;
+      in unbridgedNetworks // bridgedNetworks // bridgeNetwork; # Merge all potential networks
+    };
+
+  # use TCP BBR has significantly increased throughput and reduced latency for connections
+    boot.kernel.sysctl = {
+      "net.core.default_qdisc" = "fq";
+      "net.ipv4.tcp_congestion_control" = "bbr";
     };
   };
 }
