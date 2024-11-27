b78d03aafea516b464ad3f44fb7a3516af276e68
Xavier Lauzon <xavier@lumae.net>
2024-11-27 17:05:46 -0500

    Convert nixos-modules to a flake.

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A collection of NixOS modules";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    impermanence = {
+      url = "github:nix-community/impermanence";
+    };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    vscode-server.url = "github:nix-community/nixos-vscode-server";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-unstable, ... }@inputs:
+    {
+      nixosModules = import ./modules;
+    };
+}
