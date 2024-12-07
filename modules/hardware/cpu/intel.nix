304bdfa08f2f2c7b91a06b05148e5965b068bf98
Xavier Lauzon <xavier@lumae.net>
2024-12-07 17:17:36 -0500

    modules/hardware/cpu/intel.nix remove old boot modules & params

--- modules/hardware/cpu/intel.nix
+++ modules/hardware/cpu/intel.nix
@@ -7,10 +7,6 @@ in
   config = mkIf (device.cpu == "intel" || device.cpu == "vm-intel") {
     hardware.cpu.intel.updateMicrocode = true;
 
-    boot = {
- #     kernelModules = ["kvm-intel"];
- #     kernelParams = ["i915.fastboot=1" "enable_gvt=1"];
-    };
     host.feature.boot.kernel = {
       modules = [
         "kvm-intel"
