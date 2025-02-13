8f0ceaa27cfe2481f8dfc1d49d41b944674b4188
Xavier Lauzon <xavier@lumae.net>
2025-02-13 15:26:24 -0500

    cleanup: hyprland config

--- modules/feature/graphics/windowManager/hyprland.nix
+++ modules/feature/graphics/windowManager/hyprland.nix
@@ -9,29 +9,24 @@ in
     programs = {
       hyprland = {
         enable = mkDefault true;
-        package = pkgs.hyprland;
-        portalPackage = pkgs.xdg-desktop-portal-hyprland;
-#        package = inputs.hyprland.packages.${pkgs.system}.hyprland;
-#        portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
       };
     };
-
-    environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
     xdg.portal = {
       enable = true;
       xdgOpenUsePortal = true;
-      config.common = {
-        "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
-        "org.freedesktop.impl.portal.ScreenCast" = [ "hyprland" ];
-        "org.freedesktop.impl.portal.Screenshot" = [ "hyprland" ];
-        "org.freedesktop.portal.FileChooser" = [ "xdg-desktop-portal-shana" ];
+      config = {
+        common = {
+          default = ["gtk"];
+          "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
+        };
+        hyprland = {
+          default = ["gtk" "hyprland"];
+        };
       };
       extraPortals = with pkgs; [
-#        inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland
-        xdg-desktop-portal-hyprland
         xdg-desktop-portal-gtk
-        xdg-desktop-portal-wlr
+        xdg-desktop-portal-hyprland
       ];
     };
   };
