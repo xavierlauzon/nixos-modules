f660411df7a58894d939e3e70eee3107b8a666ff
Xavier Lauzon <xavier@lumae.net>
2025-07-02 13:22:29 -0400

    remove inputs

--- flake.nix
+++ flake.nix
@@ -1,24 +1,9 @@
 {
   description = "A collection of NixOS modules";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    impermanence = {
-      url = "github:nix-community/impermanence";
-    };
-    sops-nix = {
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    vscode-server.url = "github:nix-community/nixos-vscode-server";
-  };
+  inputs = {};
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, ... }@inputs:
+  outputs = { self, ... }@inputs:
     {
       nixosModules = import ./modules;
     };
