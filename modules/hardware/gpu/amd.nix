0ed0f7098c2962824cbc79df253dc0c97edea47f
Xavier Lauzon <xavier@lumae.net>
2026-02-17 11:34:41 -0500

    hardware/gpu/amd.nix - Add hybrid nvidia/amd GPU support

--- modules/hardware/gpu/amd.nix
+++ modules/hardware/gpu/amd.nix
@@ -4,8 +4,9 @@ let
   device = config.host.hardware ;
   backend = config.host.feature.graphics.backend;
   graphics = config.host.feature.graphics.enable;
+  isHybridNvidia = (device.gpu == "hybrid-amd-nvidia");
 in {
-  config = mkIf (device.gpu == "amd" || device.gpu == "hybrid-amd" || device.gpu == "integrated-amd")  {
+  config = mkIf (device.gpu == "amd" || device.gpu == "hybrid-amd" || device.gpu == "hybrid-amd-nvidia" || device.gpu == "integrated-amd")  {
     boot = lib.mkMerge [
       (lib.mkIf (lib.versionAtLeast pkgs.linux.version "6.2") {
         kernelModules = [
@@ -21,7 +22,6 @@ in {
       rocmPackages.rocminfo
       rocmPackages.rocm-smi
       rocmPackages.rocm-runtime
-      psi-notify
     ];
 
     hardware.enableRedistributableFirmware = true;
@@ -31,19 +31,14 @@ in {
         opencl.enable = true;
     };
 
-    environment = {
-      sessionVariables = mkMerge [
-        (mkIf (graphics) {
-          LIBVA_DRIVER_NAME = "radeonsi";
-        })
-
-        (mkIf ((graphics) && (backend == "wayland")) {
-          WLR_NO_HARDWARE_CURSORS = "1";
-        })
-      ];
+    # When paired with NVIDIA dGPU, let nvidia.nix handle LIBVA and videoDrivers
+    environment = mkIf (!isHybridNvidia) {
+      sessionVariables = mkIf (graphics) {
+        LIBVA_DRIVER_NAME = "radeonsi";
+      };
     };
 
-    services.xserver.videoDrivers = (mkIf ((graphics) && (backend == "x"))) [
+    services.xserver.videoDrivers = mkIf ((!isHybridNvidia) && (graphics) && (backend == "x")) [
       "amdgpu"
     ];
   };
